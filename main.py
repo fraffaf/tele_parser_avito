7976bac9d86bd33836d95bf40ad2714aa7806399
fraffaf <ilya.gi99@gmail.com>
2022-02-13 03:19:42 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-test
+import telebot
+
+bot = telebot.TeleBot('5215163849:AAGbR2D4qKH7iYUmQNUuBxS3S3Gh30hCuLI')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'эхо')
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, "твое сообщение"+ message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
